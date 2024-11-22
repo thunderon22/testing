ca178379a209f5fa9ee4b22af024306bf314ff3a
nagarjunaIntern <nagarjuna.gayam@coschool.ai>
2024-11-22 19:38:26 +0530

    This is my second commit

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,6 @@
 import pandas as pd
-series_data = pd.Series([1,2,3,4,5,6],index=['a','b','c','d','e','f'])
-series_data['g'] = 7
-series_data['a'] = 100
+array = [[1,2,3,4,5],[1,2,3,4,56],[3422,6,7,9,6]]
+data_frame  = pd.DataFrame(array)
+print(data_frame)
 
-print(series_data)
+
